f5e83897f50fe676cc2ee48114eafe717b418464
kor <kor@dwarshuis.com>
2024-09-09 17:49:22 +0200

    Generated

--- output/xrefs-history/xrefs-data-1725896166433.js
+++ output/xrefs-history/xrefs-data-1725896166433.js
@@ -19,8 +19,8 @@ const allXrefs = {
       "owner": "kordwarshuis",
       "repo": "spec-up-xref-test-1",
       "site": "https://kordwarshuis.github.io/spec-up-xref-test-1/",
-      "commitHash": "d086c653abea3ecc475aeb41e75239ea350e6ec0",
-      "content": "[[def: AAL]]:\n\n~ See: [[ref: authenticator assurance level]].\n\n~ This is an addition, for testing purposes.\n\n~ Addition for testing.\n\n~ Another addition for testing.\n\n~ Third addition\n\n~ Another line added for demonstration purpose.\n\n~ Another Addition for testing.\n"
+      "commitHash": null,
+      "content": null
     },
     {
       "externalSpec": "test-2",
@@ -30,8 +30,8 @@ const allXrefs = {
       "owner": "kordwarshuis",
       "repo": "spec-up-xref-test-2",
       "site": "https://kordwarshuis.github.io/spec-up-xref-test-2/",
-      "commitHash": "117b86fa676c0bd3030c16e8bbb87ebaa61af576",
-      "content": "[[def: ABAC]]:\n\n~ See: [[ref: attribute-based access control]].\n\n"
+      "commitHash": null,
+      "content": null
     }
   ]
 };
